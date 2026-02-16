8d8ee0e9f3ebd9c0aa85f7fd785ffeb016410712
premkarnawat <premkarnawat1716@gmail.com>
2026-02-16 22:38:14 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -30,9 +30,13 @@ llm_client = OpenAI(
 
 # ----------------------------------
 # Load ML Model
-# ----------------------------------
+# ---------------------------------
 
-model = joblib.load("model/burnout_model.pkl")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(BASE_DIR, "..", "model", "burnout_model.pkl")
+
+model = joblib.load(model_path)
+
 
 # ----------------------------------
 # App Init
